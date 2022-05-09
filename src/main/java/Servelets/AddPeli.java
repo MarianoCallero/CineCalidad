aeb3061fc034f7caadb53e1438f8657b1abcc279
MarianoCallero <31424377+MarianoCallero@users.noreply.github.com>
2022-05-08 21:58:40 -0300

    1.2

--- src/main/java/Servelets/AddPeli.java
+++ src/main/java/Servelets/AddPeli.java
@@ -15,29 +15,20 @@ import java.util.List;
 
 @WebServlet(name = "AddPeli", value = "/AddPeli")
 public class AddPeli extends HttpServlet {
-    PeliculaSessionBeanRemote p = new PeliculaSessionBean();
-    List<Pelicula> pelis = null;
-    HttpSession pels;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PeliculaSessionBean p = new PeliculaSessionBean();
         String l=request.getParameter("lanzamiento");
         String n = request.getParameter("nombre");
         String d = request.getParameter("descripcion");
-        p.addPeliculas(l,n,d);
-        pels=request.getSession();
-        pels.setAttribute("peliculas",p.getPeliculas());
+        HttpSession session = request.getSession();
+        session.setAttribute("pelic",p);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
       PrintWriter writer = response.getWriter();
-        //PeliculaSessionBean p = new PeliculaSessionBean();
-        String message="";
-        List<Pelicula> pp = new ArrayList<Pelicula> ();
-        pp=(ArrayList<Pelicula>) pels.getAttribute("Peliculas");
-        for(int i=0;i<pp.size();i++){
-            message = message + pp.get(i).toString() +"\n";
-        }
-        writer.println(message);
+        writer.println("Articulo Guardado");
     }
 }
